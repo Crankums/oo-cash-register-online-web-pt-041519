4ecd42ab90d6a4b724a595e5d2400e855beb8f3c
Craig Donavin <crdonavin@gmail.com>
2019-05-20 20:19:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,6 +15,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     self.total += price*quantity
     quantity.times {@items << title}
+    @prev_trans = self.total
   end
 
   def apply_discount
@@ -27,7 +28,6 @@ class CashRegister
   end
 
   def void_last_transaction
-    @total - add_item
   end
 
 end
